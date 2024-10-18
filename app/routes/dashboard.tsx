966aeefe950669140fcc9c1e10c523e4a25d17ba
iansyahr <apriansyahramadhan123@gmail.com>
2024-10-18 08:00:58 +0000

    fix linting

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,42 +1,46 @@
 import { useEffect, useState } from "react";
-import type { MetaFunction} from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
 import { getSession } from "~/utils/sessions";
 
 export const meta: MetaFunction = () => {
-    return [
-      { title: "Furina Dashboard" },
-      { name: "description", content: "just random note-taking apps" },
-    ];
-  };
+  return [
+    { title: "Furina Dashboard" },
+    { name: "description", content: "just random note-taking apps" },
+  ];
+};
 
-  export const loader = async ({ request }: { request: Request }) => {
-    const session = await getSession(request.headers.get("Cookie"));
-  
-    if (!session.has("user")) {
-      return redirect("/");
-    }
-  
-    return null;
-  };
-  
-  export default function Dashboard() {
-    const [catImage, setCatImage] = useState("");
-  
-    useEffect(() => {
-      fetch("https://cataas.com/cat?json=true")
-        .then((response) => response.json())
-        .then((data) => setCatImage(`https://cataas.com${data.url}`));
-    }, []);
-  
-    return (
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Welcome to the Dashboard!</h1>
-        {catImage ? (
-          <img src={catImage} alt="Random Cat" style={{ width: "300px", height: "300px" }} />
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    );
+export const loader = async ({ request }: { request: Request }) => {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  if (!session.has("user")) {
+    return redirect("/");
   }
+
+  return null;
+};
+
+export default function Dashboard() {
+  const [catImage, setCatImage] = useState("");
+
+  useEffect(() => {
+    fetch("https://cataas.com/cat?json=true")
+      .then((response) => response.json())
+      .then((data) => setCatImage(`https://cataas.com${data.url}`));
+  }, []);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to the Dashboard!</h1>
+      {catImage ? (
+        <img
+          src={catImage}
+          alt="Random Cat"
+          style={{ width: "300px", height: "300px" }}
+        />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
